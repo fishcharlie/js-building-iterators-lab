0dc5f8b283acc228f93a711dd4c7810bde116353
Charlie Fish <fishcharlie@me.com>
2016-06-20 15:53:31 -0600

    Homework Week 2 Day 1

--- myReduce.js
+++ myReduce.js
@@ -1,9 +1,42 @@
 // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce for more details
 // Don't worry about initialValue at first. You can always add it in later.
 
-function myReduce(arr, callback) {
+function myReduce(arr, callback, initialValue) {
 
 //  CODE INSIDE HERE   //
+	if (arr.length !== 0) {
+		var last = arr[0];
+		var current = arr[1];
+
+		for (var i = 0; i < arr.length; i++) {
+			if (initialValue) {
+				if (i === 0) {
+					last = callback(initialValue,current,i,arr);
+					current = arr[i+1];
+				}
+				else {
+					last = callback(last,current,i,arr);
+					current = arr[i+1];
+				}
+			}
+
+			else {
+				if (i === 0) {
+					last = callback(arr[0],current,i+1,arr);
+					current = arr[i+1];
+				}
+				else {
+					last = callback(last,current,i,arr);
+					current = arr[i+1];
+				}
+			}
+		}
+
+		return last;
+	}
+	else {
+		return initialValue;
+	}
 
 }
 
